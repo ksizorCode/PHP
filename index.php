8d0084d3ba0b50ff1f60d9629c27b69cb7f7b153
Miguel <miguelestebn@gmail.com>
2023-04-03 16:48:27 +0200

    1

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Índice de Cosas de clase</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Indice de contenidos de clase de PHP</h1>
+        <h2>Listado de aprendizaje y sufrimiento con Miguel Esteban</h2>
+    </header>
+
+    <main>
+        <ol class="archivos">
+            <li><a href="/echo">echo</a></li>
+            <li><a href="/funciones">funciones</a></li>
+
+            <li><a href="/ejemplo">ejemplo</a></li>
+            <li><a href="/ejemplo">ejemplo</a></li>
+            <li><a href="/ejemplo">ejemplo</a></li>
+            <li><a href="/ejemplo">ejemplo</a></li>
+            <li><a href="/ejemplo">ejemplo</a></li>
+            <li><a href="/ejemplo">ejemplo</a></li>
+        </ol>
+    </main>
+
+    <footer>
+        <p>Dicampus <?php echo date('Y');?></p>
+    </footer>
+</body>
+
+</html>
