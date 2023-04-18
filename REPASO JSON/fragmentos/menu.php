a9c4da5051fb52c87c349295fc4f9b5a45045c52
Miguel <miguelestebn@gmail.com>
2023-04-18 15:23:43 +0200

    AAS

---
+++ REPASO JSON/fragmentos/menu.php
@@ -0,0 +1,11 @@
+<ul class="menu">
+    <li>
+        <a href="index.php">Inicio</a>
+    </li>
+    <li>
+        <a href="aviso-legal.php">Aviso legal</a>
+    </li>
+    <li>
+        <a href="reset.php">Reset</a>
+    </li>
+</ul>
