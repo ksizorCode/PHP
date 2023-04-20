767c66d2faf6f7ed25830b457e646a3bd0f0b847
Miguel <miguelestebn@gmail.com>
2023-04-20 15:52:00 +0200

    df

--- PHP/8 JSON/01 JSON animal -1/cachinos/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-
-<footer>
-    <p>&copy; Copyright - La Granja de Richard <? echo date('Y')?></p>
-</footer>
-
-</body>
-</html>
--- PHP/8 JSON/4. JSON Reader/__copiaSeguridad/fragmentos/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-<footer>
-    <p>&copy; Copyright <?echo $titulo;?> - <?echo date('Y');?></p>
-</footer>
-</body>
-</html>
-<? debuggin('Footer Cargado');?>
